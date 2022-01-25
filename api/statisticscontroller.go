1288702ccf7155411999e79c2af6b812d05641af
R. Ortal <rortal@no-reply.org>
2022-01-24 16:24:48 -1000

    WIP: Verify id passed from request context

--- api/statisticscontroller.go
+++ api/statisticscontroller.go
@@ -3,6 +3,7 @@ package api
 import (
 	"cf-proposal/common/logservice"
 	"cf-proposal/common/types"
+	"log"
 	"net/http"
 
 	"github.com/go-chi/chi"
@@ -20,5 +21,7 @@ func (sc StatisticsController) StatisticsRoutes() chi.Router {
 }
 
 func (sc StatisticsController) HandleGetStatistics(w http.ResponseWriter, r *http.Request) {
+	id := r.Context().Value("id").(string)
+	log.Printf(id)
 	logservice.LogHttpRequest(http.StatusOK, r.Method, types.Path(r.URL.Path))
 }
