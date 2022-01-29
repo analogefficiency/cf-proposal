07ca1440e5441225438f2ff2a5659e52724a7653
R. Ortal <rortal@no-reply.org>
2022-01-28 23:49:41 -1000

    WIP: Added URL validation on create

--- domain/model/UrlDto.go
+++ domain/model/UrlDto.go
@@ -3,6 +3,7 @@ package model
 import (
 	"cf-proposal/common/messages"
 	"fmt"
+	"net/url"
 )
 
 type UrlDto struct {
@@ -16,6 +17,12 @@ func (dto UrlDto) ValidateCreate() error {
 
 	if dto.LongUrl == "" {
 		message := fmt.Sprintf(messages.INVALID_NULL_VALUE, "Long Url")
+		return fmt.Errorf("%s", message)
+	}
+
+	_, err := url.ParseRequestURI(dto.LongUrl)
+	if err != nil {
+		message := fmt.Sprintf(messages.INVALID_URL, dto.LongUrl)
 		return fmt.Errorf("%s", message)
 	}
 	return nil
--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.17
 require (
 	github.com/go-chi/chi v1.5.4
 	github.com/mattn/go-sqlite3 v1.14.10
+	github.com/pkg/errors v0.9.1
 )
