3a56a93c002a070485bb4cc8d282d575e62d9315
R. Ortal <rortal@no-reply.org>
2022-01-31 00:14:16 -1000

    WIP: Missing foreign key constraint and fixed commit verification

--- domain/ddl/schema/HISTORY.sql
+++ domain/ddl/schema/HISTORY.sql
@@ -1,5 +1,7 @@
 CREATE TABLE HISTORY (
 	"history_id"                   INTEGER NOT NULL PRIMARY KEY,		
 	"url_id"                       INTEGER NOT NULL,
-    "access_dt"                    TEXT NOT NULL
+    "access_dt"                    TEXT NOT NULL,
+
+	FOREIGN KEY("url_id") REFERENCES URL("url_id")
 );
